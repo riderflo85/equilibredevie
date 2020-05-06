188da6ef934298b1f0b6ccee998a5c2f4d9f456c
Florent <grenaille.florent85@gmail.com>
2020-05-06 18:02:41 +0200

    Extend user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from shop.models import Order
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(
+        _('adresse email'),
+        null=False,
+        unique=True
+    )
+    phone_number = models.IntegerField(
+        max_length=10,
+        null=False,
+        verbose_name='Numéro de téléphone'
+    )
+    adress = models.CharField(
+        max_lentgh=350,
+        null=False,
+        verbose_name='Adresse'
+    )
+    postal_code = models.IntegerField(
+        max_length=5,
+        null=False,
+        verbose_name='Code postale'
+    )
+    city = models.CharField(
+        max_lentgh=250,
+        null=False,
+        verbose_name='Ville'
+    )
+    is_verified = models.BooleanField(
+        default=False,
+        verbose_name='Compte vérifié'
+    )
+    order = models.ManyToManyField(Order)
