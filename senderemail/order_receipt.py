f404df22680db752dfb45cf40d338533319af4f0
Florent <grenaille.florent85@gmail.com>
2020-05-26 16:54:47 +0200

    Add new feature for send the email from the client

--- senderemail/order_receipt.py
+++ senderemail/order_receipt.py
@@ -1,6 +1,7 @@
 import os, requests
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Email, To, Content, Mail
+from order.models import OrderProductQuantity
 
 
 def sender_receipt(order, user):
@@ -24,3 +25,87 @@ def sender_receipt(order, user):
     response = sg.client.mail.send.post(request_body=mail.get())
 
     return str(response.status_code)
+
+def sender_validation_cmd_client(order, user, another_adress):
+    """
+    Send the email to the client for validate the order.
+    """
+
+    date_cmd = str(order.created).split(' ')[0].split('-')
+    data = {
+        "cmd": {
+            "ref": order.reference,
+            "nameclient": f"{user.last_name.upper()} {user.first_name}",
+            "adress": user.adress,
+            "postalcode": str(user.postal_code),
+            "city": user.city.upper(),
+            "phonenumber": f"0{str(user.phone_number)}",
+            "anotheradress": {},
+            "note": order.note,
+            "date": f"{date_cmd[2]}/{date_cmd[1]}/{date_cmd[0]}",
+            "totalprice": str(order.total_price),
+            "products": []
+        }
+    }
+
+    if another_adress:
+        data['cmd']['anotheradress'] = {
+            "ifyes": "true",
+            "nameclient": f"{order.last_name.upper()} {order.first_name}",
+            "adress": order.adress,
+            "postalcode": str(order.postal_code),
+            "city": order.city.upper(),
+            "phonenumber": f"0{str(order.phone_number)}",
+        }
+
+    else:
+        data['cmd']['anotheradress'] = {
+            "ifyes": "false",
+            "nameclient": "none",
+            "adress": "none",
+            "postalcode": "none",
+            "city": "none",
+            "phonenumber": "none",
+        }
+
+    for product in OrderProductQuantity.objects.filter(id_order=order):
+        data['cmd']['products'].append({
+            "name": product.id_product.name,
+            "priceunit": str(product.price),
+            "totalprice": str(product.get_price()),
+            "quantity": str(product.quantity)
+        })
+
+    url = "https://api.sendgrid.com/v3/mail/send"
+
+    payload = {
+        "personalizations": [{
+            "to": [{
+                "email": user.email,
+                "name": str(user)
+            }],
+            "dynamic_template_data": data,
+            "subject": "Validation de commande"
+        }],
+        "from": {
+            "email": os.environ.get('FROM_EMAIL'),
+            "name": os.environ.get('FROM_NAME_EMAIL')
+        },
+        "reply_to": {
+            "email": os.environ.get('FROM_EMAIL'),
+            "name": os.environ.get('FROM_NAME_EMAIL')
+        },
+        "template_id": "d-543faba5b6db4e45971c71f97f3fe243"
+    }
+
+    # payload = str(payload).replace("'", "\"")
+    headers = {
+        'authorization': f"Bearer {os.environ.get('SENDGRID_API_KEY')}",
+        'content-type': "application/json; charset=utf8"
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+
+    # import pdb; pdb.set_trace()
+    return response.text
+    # return str(payload)
--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from useraccount.models import User
+from order.models import Order, OrderProductQuantity
+from senderemail import order_receipt
+from .models import Product, Category
 
-# Create your tests here.
+
+class SendEmailTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            'TestPseudoNoReplica',
+            'supertester@tester.com',
+            'testUserPassword!',
+            civility='Mr',
+            first_name='TestFirstName',
+            last_name='TestLastName',
+            phone_number=723456789,
+            adress='134 Rue De Paris',
+            postal_code=75000,
+            city='Paris',
+        )
+        self.categ = Category.objects.create(
+            name='categ test',
+            slug='categ_test'
+        )
+        self.product = Product.objects.create(
+            category = self.categ,
+            name='test bicar',
+            slug='test_bicar',
+            price=12.22,
+            available=True,
+            priority='1',
+        )
+    
+    def test_send_email_confirm_cmd(self):
+        order = Order.objects.create(
+            reference="18052020172922666298",
+            last_name="Grenaille",
+            first_name="Florent",
+            adress="Route de la Bêchée",
+            postal_code=85300,
+            city="Challans",
+            dep="vendée",
+            phone_number=789651420,
+            email="florent@gmail.com",
+            url_receipt="https://url_de_recu_de_commande.com",
+            total_price=12.22,
+            note=' ',
+            
+        )
