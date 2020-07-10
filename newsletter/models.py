1495a143b3149c05901f70fef9944060e1a43119
Florent <grenaille.florent85@gmail.com>
2020-07-10 07:11:13 +0200

    Add an model newsletter

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsLetter(models.Model):
+    title = models.CharField(
+        max_length=50,
+        verbose_name='titre de la newsletter'
+    )
+    body = models.TextField(verbose_name='contenu de la newsletter')
+    send = models.BooleanField(verbose_name='envoyé', default=False)
+    created = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='créer le'
+    )
+    updated = models.DateTimeField(
+        auto_now=True,
+        verbose_name='mis à jour le'
+    )
+
+    class Meta:
+        ordering = ('created',)
+        verbose_name = 'newsletter'
+        verbose_name_plural = 'newsletters'
+
+    def __str__(self):
+        return f"Newsletter du {self.created}"
